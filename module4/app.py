e37cfdea53f0cee5fe046d1c99f1cba8eddf85b9
Mary Anna Kivenson <mkivenson@gmail.com>
2019-10-27 22:31:23 -0400

    Update app.py

--- module4/app.py
+++ module4/app.py
@@ -27,6 +27,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 colors = {
     'background': '#F8F8FF',
     'view_bg': 'white',
