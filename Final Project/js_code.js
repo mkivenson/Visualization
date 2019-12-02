44f27419e370d6a976be96c064b612357c9f792c
Mary Anna Kivenson <mkivenson@gmail.com>
2019-12-01 23:36:24 -0500

    final project work

--- Final Project/js_code.js
+++ Final Project/js_code.js
@@ -32,38 +32,87 @@ function rowMatch(a, m) {
 }
 
 docReady(() => {
-  const graph = document.querySelector('#graph');
+  const main_graph = document.querySelector('#main_graph');
+  const graph_naturalgas = document.querySelector('#graph_naturalgas');
+  const graph_petroleum = document.querySelector('#graph_petroleum');
+  const graph_electricity = document.querySelector('#graph_electricity');
+  const graph_coal = document.querySelector('#graph_coal');
   const myDataFile = 'https://raw.githubusercontent.com/mkivenson/Visualization/master/Final%20Project/Datasets/State%20Energy%20Data%20System%201960-2009/preprocessed.csv';
   var filters = {
     'Year': '1970',
-    'Energy Type': 'Coal',
+    'Energy Type': 'Total energy',
     'Measure': 'consum',
     'Unit': 'Billion Btu',
     'Sector': 'transportation'
   };
 
   var slider = document.getElementById("myRange");
-  const year_output = document.querySelector('#slider_output');
+  const year_output = document.querySelector('#year_output');
   const type_output = document.querySelector('#type_filt');
   const sector_output = document.querySelector('#sector_filt');
-  
-  let chart1_data = []
+  const screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  const stretch_height = document.querySelector('.page-stretcher').clientHeight;
+  const contain = document.querySelector('.flex-container-main');
+  var controller = new ScrollMagic.Controller({
+    triggerElement: contain
+    });
+
+	// build scene
+
+  var slides = document.querySelectorAll("section.panel");
+
+  // create scene for every slide
+  for (var i=0; i<slides.length; i++) {
+    new ScrollMagic.Scene({
+      duration: stretch_height - 100,
+      offset: stretch_height*i
+      })
+      .setPin(contain)
+      .setTween(new TimelineMax()
+        .to('.section-container', 1, {x: `-${(i*25)}%`}))
+      .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+  }
+
+  new ScrollMagic.Scene(
+    {duration: stretch_height - 100}
+  )
+        .setPin(contain)
+        .on("progress", function (e) {
+          console.log('start')
+          var scroll_year = 1960 + Math.round((49 * e.progress), 0)
+          year_output.innerHTML = scroll_year;
+          filters = {
+            ...filters,
+            'Year': scroll_year.toString(),
+          };
+          var filtered_data = chart_filter(chart_data, filters)
+          console.log('end')
+          chart_plot(filtered_data, main_graph, 'Total Energy Consumption by State')
+          //chart_subplots(chart_data, filters)
+          
+        })
+        
+        .addTo(controller)
+        .addIndicators();
+        
+  let chart_data = []
   Plotly.d3.csv(
     myDataFile,
     (inputData) => {
-      chart1_data = inputData
+      chart_data = inputData
       // setup input fields
       // slider setup
-      const years = Array.from(new Set(unpack(inputData, 'Year')))
-      slider.min = Math.min(...years)
-      slider.max = Math.max(...years)
-      slider.value = slider.min
-      year_output.innerHTML = slider.value
+      // const years = Array.from(new Set(unpack(inputData, 'Year')))
+      // slider.min = Math.min(...years)
+      // slider.max = Math.max(...years)
+      // slider.value = slider.min
+      // year_output.innerHTML = slider.value
 
-      // type_filter
-      Array.from(new Set(unpack(inputData, 'Energy Type')))
-      .map((t) => createElementFromHTML(`<option value="${t}">${t}</option>`))
-      .forEach( (e) => type_output.appendChild(e))
+      // // type_filter
+      // Array.from(new Set(unpack(inputData, 'Energy Type')))
+      // .map((t) => createElementFromHTML(`<option value="${t}">${t}</option>`))
+      // .forEach( (e) => type_output.appendChild(e))
 
       // sector_filter
       Array.from(new Set(unpack(inputData, 'Sector')))
@@ -72,46 +121,49 @@ docReady(() => {
 
 
       // generate plot
-      var filtered_data = chart1_filter(chart1_data, filters)
-      chart1_plot(filtered_data, graph)
+      var filtered_data = chart_filter(chart_data, filters)
+      chart_plot(filtered_data, main_graph, 'Total Energy Consumption by State')
+      chart_subplots(chart_data, filters)
     }
   );
 
   // Update the current slider value (each time you drag the slider handle)
-  slider.onchange = function() {
-    year_output.innerHTML = slider.value;
-    filters = {
-      ...filters,
-      'Year': slider.value,
-    };
-    var filtered_data = chart1_filter(chart1_data, filters)
-    chart1_plot(filtered_data, graph)
-  } 
+  // slider.onchange = function() {
+  //   year_output.innerHTML = slider.value;
+  //   filters = {
+  //     ...filters,
+  //     'Year': slider.value,
+  //   };
+  //   var filtered_data = chart_filter(chart_data, filters)
+  //   chart_plot(filtered_data, main_graph, 'Total Energy Consumption by State')
+  //   chart_subplots(chart_data, filters)
+  // } 
 
-  type_output.onchange = function() {
+  // type_output.onchange = function() {
     
-    filters = {
-      ...filters,
-      'Energy Type': type_output.options[type_output.selectedIndex].value,
-    };
-    var filtered_data = chart1_filter(chart1_data, filters)
-    chart1_plot(filtered_data, graph)
-  } 
+  //   filters = {
+  //     ...filters,
+  //     'Energy Type': type_output.options[type_output.selectedIndex].value,
+  //   };
+  //   var filtered_data = chart_filter(chart_data, filters)
+  //   chart_plot(filtered_data, main_graph, 'Total Energy Consumption by State')
+  // } 
 
   sector_output.onchange = function() {
     filters = {
       ...filters,
       'Sector': sector_output.options[sector_output.selectedIndex].value,
     };
-    var filtered_data = chart1_filter(chart1_data, filters)
-    chart1_plot(filtered_data, graph)
+    var filtered_data = chart_filter(chart_data, filters)
+    chart_plot(filtered_data, main_graph, 'Total Energy Consumption by State')
+    chart_subplots(chart_data, filters)
   }
-  
+
 });
 
 
 
-function chart1_filter (inputData, filters) {
+function chart_filter (inputData, filters) {
   return inputData.map((row) => ({
     ...row,
     'Data': parseFloat(row['Data'])
@@ -120,7 +172,9 @@ function chart1_filter (inputData, filters) {
   .filter((row) => !rowMatch(row, {'StateCode': 'US'}))
 }
 
-function chart1_plot (inputData, location, title) {
+function chart_plot (inputData, location, title) {
+  const screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  const screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     const data = [{
       type: 'choropleth',
       locationmode: 'USA-states',
@@ -129,18 +183,71 @@ function chart1_plot (inputData, location, title) {
       text: unpack(inputData, 'Description'),
       zmin: Math.min(...unpack(inputData, 'Data')),
       zmax: Math.max(...unpack(inputData, 'Data')),
-      colorscale: 'Viridis'
+      colorscale: 'Viridis',
+      colorbar: {
+        title: 'Billions BTU',
+        thickness: 25,
+        
+      },
   }];
 
   const layout = {
-      title: 'Energy Consumption by State (in billions of BTU)',
+      title: {
+        text:title,
+        font: {
+          family: 'Calibri',
+          size: 24
+        },
+        xref: 'paper',
+        x: 0.05,
+      },
+      width: screenWidth / 3, 
+      height: screenHeight / 2,
+      dragmode: false,
       geo:{
           scope: 'usa',
           showlakes: true,
           lakecolor: 'rgb(255,255,255)'
-      }
+      },
   };
 
-  Plotly.react(location, data, layout, {showLink: false});
+  Plotly.react(location, data, layout, {showLink: false, responsive: false});
 }
+
+function chart_subplots(chart_data, filter){
+  /*coal data and chart*/
+  var filters_coal = {
+    ...filter,
+    'Energy Type': 'Coal'
+  }
+  var coal_data = chart_filter(chart_data, filters_coal)
+  chart_plot(coal_data, graph_coal, 'Coal Consumption by State')
+
+  /*electricity data and chart*/
+  var filters_electricity = {
+    ...filter,
+    'Energy Type': 'Electricity'
+  }
+  var elec_data = chart_filter(chart_data, filters_electricity)
+  chart_plot(elec_data, graph_electricity, 'Electricity Consumption by State')
+
+  /*natural gas data and chart*/
+  var filters_naturalgas =  {
+    ...filter,
+    'Energy Type': 'Natural gas'
+  }
+  var natgas_data = chart_filter(chart_data, filters_naturalgas)
+  chart_plot(natgas_data, graph_naturalgas, 'Natural Gas Consumption by State')
+
+  /*petroleum data and chart*/
+  var filters_petroleum = {
+    ...filter,
+    'Energy Type': 'All petroleum products'
+  }
+  var petroleum_data = chart_filter(chart_data, filters_petroleum)
+  chart_plot(petroleum_data, graph_petroleum, 'Petroleum Consumption by State')
+}
+
+
+
 
